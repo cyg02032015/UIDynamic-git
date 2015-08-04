698a50e0e13f5c89aa2be79afd2046948e134cbf
youngKook <youngKook1992@163.com>
2015-08-04 17:20:18 +0800

    Initial Commit

---
+++ UIDynamicDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIDynamicDemo
+//
+//  Created by C on 15/8/4.
+//  Copyright (c) 2015年 SiQian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
